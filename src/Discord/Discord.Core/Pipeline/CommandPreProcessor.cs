10c45e8794772f16ec280de0be44504694d14dc2
Al'ciel Geneirodan <geneirodanjekin@gmail.com>
2025-10-29 21:48:34 +0100

    Hotfix/Fix logger deadlock

--- src/Discord/Discord.Core/Pipeline/CommandPreProcessor.cs
+++ src/Discord/Discord.Core/Pipeline/CommandPreProcessor.cs
@@ -10,11 +10,16 @@ public sealed partial class CommandPreProcessor<TRequest>(ILogger<TRequest> logg
 {
     public Task Process(TRequest request, CancellationToken cancellationToken)
     {
-        using (LogContext.PushProperty("Request", request, destructureObjects: true))
-            LogCommand(typeof(TRequest).Name, request.Context.FullCommandName);
+        LogCommand(
+            typeof(TRequest).Name,
+            request.Context.FullCommandName,
+            request.Context.Member?.ToString(),
+            request.Context.Guild?.ToString()
+        );
         return Task.CompletedTask;
     }
 
-    [LoggerMessage(LogLevel.Information, "Executing command: {CommandName} with call \"{Text}\"")]
-    private partial void LogCommand(string commandName, string? text);
+    [LoggerMessage(LogLevel.Information,
+        "Executing command: {CommandName} with call \"/{Text}\" by {Member} in {Guild}")]
+    private partial void LogCommand(string commandName, string? text, string? member, string? guild);
 }
